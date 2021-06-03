a7fafa8606c8b4970148b4f6e3cf82092bebe1ce
infopek <10saero01@gmail.com>
2021-06-03 12:47:14 +0200

    Initial commit

---
+++ HeadsTails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HeadsTails
+{
+    class Program
+    {
+        // Basic heads / tails coin flipper
+        static void Main(string[] args)
+        {
+            Flip();
+
+            Console.ReadLine();
+        }
+
+        static string Flip()
+        {
+            // returns either "Heads" or "Tails" with equal probability
+            Random rand = new Random();
+            int choice = rand.Next(0, 2);
+            string side;
+
+            if (choice == 0)
+            {
+                side = "Heads";
+            }
+            else
+            {
+                side = "Tails";
+            }
+
+            return side
+        }
+    }
+}
